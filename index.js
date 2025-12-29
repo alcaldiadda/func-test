c27541cfc6c00420e2daa6ad910972fee7d3a166
gepd <guillermoepd@hotmail.com>
2025-12-29 09:19:10 -0300

    Update Appwrite client configuration to use process.env

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ import { Client, Databases } from "node-appwrite";
 
 export default async ({ req, res, log }) => {
   const client = new Client();
-  client.setEndpoint(Bun.env["APPWRITE_FUNCTION_API_ENDPOINT"]);
-  client.setProject(Bun.env["APPWRITE_FUNCTION_PROJECT_ID"]);
+  client.setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT);
+  client.setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID);
   client.setKey(req.headers["x-appwrite-key"]);
 
   const databases = new Databases(client);
 
   const listado = await databases.listDocuments({
-      databaseId: "root",
-      collectionId: "time",
-    });
-  
-    log({ listado });
-  
-  return res.json({ok: true });
+    databaseId: "root",
+    collectionId: "time",
+  });
+
+  log({ listado });
+
+  return res.json({ ok: true });
 };
